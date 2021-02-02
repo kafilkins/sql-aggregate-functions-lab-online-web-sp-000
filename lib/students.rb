83c2392d5c79a6102eae9affa95a630a645f593f
Keith Filkins <kafilkins@me.com>
2021-02-02 01:57:12 +0000

    Automatically backed up by Learn

--- lib/students.rb
+++ lib/students.rb
@@ -1,7 +1,7 @@
 ## Code your solution below. Note that your SQL queries should be in quotation marks.
 
 def highest_student_gpa
-  "SELECT MAX(gpa) FROM aggregate functions;"
+  "SELECT MAX(gpa) FROM aggregate functions sql;"
 end
 
 def lowest_student_gpa
